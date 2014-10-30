549594eeb796d009021225127dcb037ace25b06a
AlexMaxHorkun <mindkilleralexs@gmail.com>
2014-10-30 12:13:35 +0200

    add-website

--- xanderhorkunspider/web/websites/urls.py
+++ xanderhorkunspider/web/websites/urls.py
@@ -8,4 +8,5 @@ from xanderhorkunspider.web.websites import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index_view, name='index'),
+                       url(r'^add-website$', views.add_website_view, name='add_website')
 )
--- xanderhorkunspider/web/websites/views.py
+++ xanderhorkunspider/web/websites/views.py
@@ -17,3 +17,7 @@ def index_view(request):
     websites_domain = domain.Websites(dao.PageDao(), models.WebsitesDBDao(), dao.LoadingDao())
     websites = websites_domain.find_websites()
     return shortcuts.render_to_response('websites/index.html', {'websites': websites})
+
+def add_website_view(request):
+    template='websites/add_website.html';
+    return shortcuts.render_to_response(template);
