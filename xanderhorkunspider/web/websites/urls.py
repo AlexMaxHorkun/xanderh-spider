b2e91d774de2c866f64292d5f7867e0035b8cdda
AlexMaxHorkun <mindkilleralexs@gmail.com>
2015-01-14 12:07:01 +0200

    just doing god's work

--- xanderhorkunspider/web/websites/urls.py
+++ xanderhorkunspider/web/websites/urls.py
@@ -3,20 +3,21 @@ __email__ = 'mindkilleralexs@gmail.com'
 
 from django.conf.urls import patterns, url
 
-from xanderhorkunspider.web.websites import views
+from xanderhorkunspider.web.websites.views import websites, auth
 
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index_view, name='index'),
-                       url(r'^add-website$', views.edit_website_view, name='add_website'),
-                       url(r'^edit-website/(?P<wid>\d+)$', views.edit_website_view, name='edit_website'),
-                       url(r'^delete_website/(?P<wid>\d+)$', views.delete_website_view, name='delete_website'),
-                       url(r'^add-page', views.edit_page_view, name='add_page'),
-                       url(r'^website/(?P<wid>\d+)/add-page', views.edit_page_view, name='add_page_to_website'),
-                       url(r'^edit-page/(?P<pid>\d+)', views.edit_page_view, name='edit_page'),
-                       url(r'^delete_page/(\d+)$', views.delete_page_view, name='delete_page'),
-                       url(r'^spider_session/webiste-(?P<wid>\d+)$', views.spider_session_view, name='spider_session'),
-                       url(r'^spider_session$', views.start_spider_session_view, name='start_spider_session'),
-                       url(r'^spider-status/(.+)$', views.spider_status_view, name='spider_status'),
-                       url(r'^sign-up$', views.signup_view, name='signup')
+                       url(r'^$', websites.index_view, name='index'),
+                       url(r'^add-website$', websites.edit_website_view, name='add_website'),
+                       url(r'^edit-website/(?P<wid>\d+)$', websites.edit_website_view, name='edit_website'),
+                       url(r'^delete_website/(?P<wid>\d+)$', websites.delete_website_view, name='delete_website'),
+                       url(r'^add-page', websites.edit_page_view, name='add_page'),
+                       url(r'^website/(?P<wid>\d+)/add-page', websites.edit_page_view, name='add_page_to_website'),
+                       url(r'^edit-page/(?P<pid>\d+)', websites.edit_page_view, name='edit_page'),
+                       url(r'^delete_page/(\d+)$', websites.delete_page_view, name='delete_page'),
+                       url(r'^spider_session/webiste-(?P<wid>\d+)$', websites.spider_session_view,
+                           name='spider_session'),
+                       url(r'^spider_session$', websites.start_spider_session_view, name='start_spider_session'),
+                       url(r'^spider-status/(.+)$', websites.spider_status_view, name='spider_status'),
+                       url(r'^sign-up$', auth.signup_view, name='signup')
 )
--- xanderhorkunspider/web/websites/views/websites.py
+++ xanderhorkunspider/web/websites/views/websites.py
@@ -6,8 +6,6 @@ import base64
 
 from django import shortcuts
 from django import http
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
 from django import template as django_template
 
 from xanderhorkunspider.web.websites import models
@@ -171,26 +169,3 @@ def spider_status_view(request, sid):
         if int(request.GET['stop_when_done']) != 0:
             spider_manager.stop_when_done = True
     return http.HttpResponse(json.dumps(response_data), content_type="application/json")
-
-
-# Auth related views
-
-def signup_view(request):
-    if request.user.is_authenticated():
-        user = request.user
-    else:
-        user = None
-    if request.method == 'POST':
-        form = forms.SignupForm(request.POST)
-        if form.is_valid():
-            User.objects.create_user(form.cleaned_data['username'], email=form.cleaned_data['email'],
-                                     password=form.cleaned_data['password'])
-            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
-            if user is not None and user.is_active:
-                login(request, user)
-                return shortcuts.redirect('index')
-            else:
-                raise RuntimeError("Unable to create user or authenticate")
-    else:
-        form = forms.SignupForm()
-    return shortcuts.render(request, "websites/auth/signup.html", {'form': form})
